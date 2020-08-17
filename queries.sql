44681e2bcd366413caef1254508a2478e7ae17da
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-08-17 11:16:28 -0600

    Homework complete. Onto bonus

--- queries.sql
+++ queries.sql
@@ -35,3 +35,44 @@ inner join departments on
 dept_emp.dept_no = departments.dept_no;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select employees.first_name,
+	employees.last_name,
+	employees.sex
+from employees
+where employees.first_name = 'Hercules'
+and employees.last_name like 'B%'
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select departments.dept_name,
+	employees.emp_no,
+	employees.first_name,
+	employees.last_name
+from employees
+inner join dept_emp on
+employees.emp_no = dept_emp.emp_no
+inner join departments on 
+dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select departments.dept_name,
+	employees.emp_no,
+	employees.first_name,
+	employees.last_name
+from employees
+inner join dept_emp on
+employees.emp_no = dept_emp.emp_no
+inner join departments on 
+dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales'
+or departments.dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select employees.last_name, count(employees.last_name)
+from employees
+group by employees.last_name
+order by count(employees.last_name) desc;
+
+
+
+
